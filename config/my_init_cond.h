ddc025e303188a4bd2746474fc5985f6f25960d5
Kazuho FUJII <kazuho.fujii@gmail.com>
2014-11-01 09:03:15 -0700

    Add my_init_cond.h

---
+++ config/my_init_cond.h
@@ -0,0 +1,38 @@
+/*
+ * Rewrite for your system.
+ */
+
+#ifndef MY_INIT_COND_H_
+#define MY_INIT_COND_H_
+
+static double init_cond_elect_x(double x, double y, double z)
+{
+	return 1.0;
+}
+
+static double init_cond_elect_y(double x, double y, double z)
+{
+	return 1.0;
+}
+
+static double init_cond_elect_z(double x, double y, double z)
+{
+	return 1.0;
+}
+
+static double init_cond_magnt_x(double x, double y, double z)
+{
+	return 1.0;
+}
+
+static double init_cond_magnt_y(double x, double y, double z)
+{
+	return 1.0;
+}
+
+static double init_cond_magnt_z(double x, double y, double z)
+{
+	return 1.0;
+}
+
+#endif /* MY_INIT_COND_H_ */
